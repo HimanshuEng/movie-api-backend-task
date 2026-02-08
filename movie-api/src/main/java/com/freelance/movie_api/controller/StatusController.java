9dc138b46f8c27e946e51b2298765fb0daa6f834
Himanshu <152671270+HimanshuEng@users.noreply.github.com>
2026-02-08 22:06:55 +0530

    Add files via upload

---
+++ movie-api/src/main/java/com/freelance/movie_api/controller/StatusController.java
@@ -0,0 +1,17 @@
+package com.freelance.movie_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Marks this class as REST Controller (returns JSON/String response)
+@RequestMapping("checkstatus")
+public class StatusController {
+
+    // GET API -> Used to check if application is running or not
+    // URL: GET /checkstatus
+    @GetMapping
+    public String checkStatus(){
+        return "Application Run successfully"; // Simple message response for testing server health
+    }
+}
---
+++ movie-api/src/main/java/com/freelance/movie_api/entity/Movie.java
@@ -0,0 +1,57 @@
+package com.freelance.movie_api.entity;
+
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private String description;
+    private String genre;
+    private Long rating;
+
+    public Movie() {
+    }
+
+    // Parameterized constructor
+    public Movie(int id, String title, String description, String genre, Long rating) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+    //Getter Setter
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public Long getRating() {
+        return rating;
+    }
+    public void setRating(Long rating) {
+        this.rating = rating;
+    }
+}

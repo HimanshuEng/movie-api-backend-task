433409f328adc01304985f6e3a1e4a64e86cae68
Himanshu <152671270+HimanshuEng@users.noreply.github.com>
2026-02-27 18:10:13 +0530

    Remove Merge Conflicts

--- src/main/java/com/freelance/movie_api/exception/MovieNotFoundException.java
+++ src/main/java/com/freelance/movie_api/exception/MovieNotFoundException.java
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
+
 package com.freelance.movie_api.exception;
+
 
 public class MovieNotFoundException extends RuntimeException {
 
@@ -7,13 +8,3 @@ public class MovieNotFoundException extends RuntimeException {
         super(message);
     }
 }
-=======
-package com.freelance.movie_api.exception;
-
-public class MovieNotFoundException extends RuntimeException {
-
-    public MovieNotFoundException(String message){
-        super(message);
-    }
-}
->>>>>>> 86fa909a0d62f6b9ca5feb7c6be4b8fa0ca916c8

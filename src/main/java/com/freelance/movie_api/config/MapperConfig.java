cadb9ae00f8ad0e1fea116fe7e8eb6a85f208eac
Himanshu <152671270+HimanshuEng@users.noreply.github.com>
2026-02-27 18:05:45 +0530

    Remove Merge Conflicts

--- src/main/java/com/freelance/movie_api/config/MapperConfig.java
+++ src/main/java/com/freelance/movie_api/config/MapperConfig.java
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 package com.freelance.movie_api.config;
 
 import org.modelmapper.ModelMapper;
@@ -13,19 +14,4 @@ public class MapperConfig {
         return new ModelMapper();
     }
 }
-=======
-package com.freelance.movie_api.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MapperConfig {
-
-    @Bean
-    public ModelMapper modelmapper(){
-        return new ModelMapper();
-    }
-}
->>>>>>> 86fa909a0d62f6b9ca5feb7c6be4b8fa0ca916c8
